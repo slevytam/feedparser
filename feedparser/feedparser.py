3b5a7ce0adbb63cafb3c477f5fa9169b868f638d
slevytam <shalom@the10thfloor.com>
2013-06-19 22:47:40 -0300

    Update feedparser.py
    for any

--- feedparser/feedparser.py
+++ feedparser/feedparser.py
@@ -2441,15 +2441,15 @@ class _HTMLSanitizer(_BaseHTMLProcessor):
 		print "tag in self.possible_elements"
 		tag_attrs = self.normalize_attrs(attrs)
 		print "tag_attrs"
-		if 'href' in tag_attrs:
+		if any('href' x for x in tag_attrs):
 			print "if href"
 			parsed_link=urlparse.urlparse(_makeSafeAbsoluteURI(tag_attrs['href']))
 			print "after parsed"
-		elif 'src' in tag_attrs:
+		elif any('src' x for x in tag_attrs):
 			print "if src"
 			parsed_link=urlparse.urlparse(_makeSafeAbsoluteURI(tag_attrs['srf']))
 			print "after src parsed"
-		elif 'data' in tag_attrs:
+		elif any('data' x for x in tag_attrs):
 			print "if data"
 			parsed_link=urlparse.urlparse(_makeSafeAbsoluteURI(tag_attrs['data']))
 			print "after data parsed"
@@ -2532,11 +2532,11 @@ class _HTMLSanitizer(_BaseHTMLProcessor):
     	if tag in self.possible_elements:
     		print "tag in self.possible_elements end"
 		tag_attrs = self.normalize_attrs(attrs)
-		if 'href' in tag_attrs:
+		if  any('href' in x for x in tag_attrs):
 			parsed_link=urlparse.urlparse(_makeSafeAbsoluteURI(tag_attrs['href']))
-		elif 'src' in tag_attrs:
+		elif any('src' in x for x in tag_attrs):
 			parsed_link=urlparse.urlparse(_makeSafeAbsoluteURI(tag_attrs['srf']))
-		elif 'data' in tag_attrs:
+		elif any('data' in x for x in tag_attrs):
 			parsed_link=urlparse.urlparse(_makeSafeAbsoluteURI(tag_attrs['data']))
 		else:
 			print "UNKNOWN ENDTAG", tag
